a177d96ae594191b94115d3c88a4f981fb4ef384
Tareque Hossain <tareque@codexn.com>
2012-08-29 13:04:55 -0700

    Added remote form.

--- src/adminapi/apps/adminapi/urls.py
+++ src/adminapi/apps/adminapi/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     url(r'^apps/?$', 'adminapi.apps.adminapi.views.get_models', name='adminapi_get_all_models'),
     url(r'^apps/(?P<app_label>\w+)/models/$', 'adminapi.apps.adminapi.views.get_models', name='adminapi_get_models'),
     url(r'^apps/(?P<app_label>\w+)/models/(?P<model_name>\w+)/instances/$', 'adminapi.apps.adminapi.views.get_model_instances', name='adminapi_get_model_instances'),
-    url(r'^apps/(?P<app_label>\w+)/models/(?P<model_name>\w+)/instances/((?P<instance_id>\d+)/)?form/$', 'adminapi.apps.adminapi.views.handle_instance_form', name='adminapi_handle_instance_form'),
+    url(r'^apps/(?P<app_label>\w+)/models/(?P<model_name>\w+)/instances/form/$', 'adminapi.apps.adminapi.views.handle_instance_form', name='adminapi_handle_instance_add_form'),
+    url(r'^apps/(?P<app_label>\w+)/models/(?P<model_name>\w+)/instances/(?P<instance_id>\d+)/form/$', 'adminapi.apps.adminapi.views.handle_instance_form', name='adminapi_handle_instance_edit_form'),
 )
--- src/adminapi/apps/adminapi/views.py
+++ src/adminapi/apps/adminapi/views.py
@@ -1,8 +1,11 @@
 from django.contrib.admin.sites import site
 from django.core.serializers.json import simplejson as json
 from django.core.urlresolvers import reverse, NoReverseMatch
+from django.forms import ModelForm
 from django.http import Http404, HttpResponse
 from django.utils.text import capfirst
+
+from django_remote_forms.forms import RemoteForm
 
 
 def handle_login(request):
@@ -44,7 +47,8 @@ def get_models(request, app_label=None):
             # If so, add the module to the model_list.
             if True in perms.values():
                 model_dict = {
-                    'name': unicode(capfirst(model._meta.verbose_name_plural)),
+                    'name': model_name,
+                    'title': unicode(capfirst(model._meta.verbose_name_plural)),
                     'perms': perms,
                 }
                 if perms.get('change', False):
@@ -67,7 +71,6 @@ def get_models(request, app_label=None):
                     app_dict[current_app_label] = {
                         'name': current_app_label.title(),
                         'title': '%s administration' % capfirst(current_app_label),
-                        'app_url': '',
                         'has_module_perms': has_module_perms,
                         'models': [model_dict],
                     }
@@ -75,7 +78,7 @@ def get_models(request, app_label=None):
         app_dict[current_app_label]['models'].sort(key=lambda x: x['name'])
 
     if not app_dict:
-        raise Http404('The requested admin page does not exist.')
+        raise Http404('The requested admin page does not exist')
 
     response_data = {
         'app_list': [app_dict],
@@ -84,19 +87,73 @@ def get_models(request, app_label=None):
     return HttpResponse(json.dumps(response_data), mimetype="application/json")
 
 
-def get_model_instances(request, model_name):
+def get_model_instances(request, app_label, model_name):
     # Return list of instances for a given model
-    pass
+    response_data = {
+        'app_label': app_label,
+        'model_name': model_name,
+        'instances': []
+    }
+
+    for model, model_admin in site._registry.items():
+        if app_label != model._meta.app_label or model_name != model._meta.module_name:
+            continue
+
+        for model_instance in model.objects.all():
+            response_data['instances'].append({
+                'id': model_instance.pk,
+                'title': unicode(model_instance),
+                'add_url': reverse('adminapi_handle_instance_add_form', args=[app_label, model_name]),
+                'edit_url': reverse('adminapi_handle_instance_edit_form', kwargs={
+                    'app_label': app_label,
+                    'model_name': model_name,
+                    'instance_id': model_instance.pk
+                }),
+                'delete_url': ''
+            })
+
+    return HttpResponse(json.dumps(response_data), mimetype="application/json")
 
 
-def handle_instance_form(request, model_name, instance_id):
-    if request.method == 'GET':
-        # Return instance form for given model name
-        # Return initial values if instance ID is supplied, otherwise return empty form
-        pass
-    elif request.method == 'POST':
-        # Create new instance for given data
-        pass
-    elif hasattr(request, 'raw_post_data'):
-        # PUT data available, update instance
-        pass
+def handle_instance_form(request, app_label, model_name, instance_id=None):
+    response_data = {
+        'app_label': app_label,
+        'model_name': model_name
+    }
+
+    instance = None
+
+    for model, model_admin in site._registry.items():
+        if app_label != model._meta.app_label or model_name != model._meta.module_name:
+            continue
+
+        if instance_id is not None:
+            response_data[instance_id] = instance_id
+            try:
+                instance = model.objects.get(pk=instance_id)
+            except model.DoesNotExist:
+                raise Http404('Invalid instance ID')
+
+        current_model = model
+
+        class CurrentModelForm(ModelForm):
+            class Meta:
+                model = current_model
+
+        if request.method == 'GET':
+            # Return instance form for given model name
+            # Return initial values if instance ID is supplied, otherwise return empty form
+            if instance is None:
+                form = CurrentModelForm()
+            else:
+                form = CurrentModelForm(instance=instance)
+            remote_form = RemoteForm(form)
+            response_data['form'] = remote_form.as_dict()
+        elif request.method == 'POST':
+            # Create new instance for given data
+            pass
+        elif hasattr(request, 'raw_post_data'):
+            # PUT data available, update instance
+            pass
+
+    return HttpResponse(json.dumps(response_data), mimetype="application/json")
